46fa91c333af1267adfddc4f6ab2db6e250490b2
ws <shu.wang@greencloud.com.cn>
2023-05-15 11:27:28 +0800

    Api Signature

--- simple-server/src/main/java/com/simple/config/RestWebConfiguration.java
+++ simple-server/src/main/java/com/simple/config/RestWebConfiguration.java
@@ -1,8 +1,7 @@
 package com.simple.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.simple.handler.RestAuthFilterHandler;
-import com.simple.service.ApiWhitelistService;
-import com.simple.service.ResourceService;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,19 +10,16 @@ import org.springframework.core.Ordered;
 @Configuration
 public class RestWebConfiguration {
 
-    private final ResourceService resourceService;
+    private final ObjectMapper objectMapper;
 
-    private final ApiWhitelistService apiWhitelistService;
-
-    public RestWebConfiguration(ResourceService resourceService, ApiWhitelistService apiWhitelistService) {
-        this.resourceService = resourceService;
-        this.apiWhitelistService = apiWhitelistService;
+    public RestWebConfiguration(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
     }
 
     @Bean
     public FilterRegistrationBean<RestAuthFilterHandler> restAuthFilter() {
         FilterRegistrationBean<RestAuthFilterHandler> filterRegBean = new FilterRegistrationBean<>();
-        filterRegBean.setFilter(new RestAuthFilterHandler(resourceService, apiWhitelistService));
+        filterRegBean.setFilter(new RestAuthFilterHandler(objectMapper));
         filterRegBean.addUrlPatterns("/rest/*");
         filterRegBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return filterRegBean;
--- simple-server/src/main/java/com/simple/handler/RestAuthFilterHandler.java
+++ simple-server/src/main/java/com/simple/handler/RestAuthFilterHandler.java
@@ -1,12 +1,11 @@
 package com.simple.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.simple.common.bean.AuthUser;
 import com.simple.common.bean.ResponseData;
 import com.simple.common.enumerate.RequestHeader;
 import com.simple.common.utils.TokenUtils;
-import com.simple.service.ApiWhitelistService;
-import com.simple.service.ResourceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.AntPathMatcher;
@@ -19,9 +18,13 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * description:  <br>
@@ -31,17 +34,13 @@ import java.util.HashSet;
 @Slf4j
 public class RestAuthFilterHandler extends AntPathMatcher implements Filter {
 
-    final ResourceService resourceService;
-    
-    final ApiWhitelistService apiWhitelistService;
-
     final HashSet<String> excludePath;
-    
-    
 
-    public RestAuthFilterHandler(ResourceService resourceService, ApiWhitelistService apiWhitelistService) {
-        this.resourceService = resourceService;
-        this.apiWhitelistService = apiWhitelistService;
+    private final ObjectMapper objectMapper;
+
+
+    public RestAuthFilterHandler(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
         this.excludePath = new HashSet<>();
         this.excludePath.add("/rest/member/wechat/info");
         this.excludePath.add("/rest/member/wechat");
@@ -49,6 +48,7 @@ public class RestAuthFilterHandler extends AntPathMatcher implements Filter {
 
     /**
      * request filter and Authorization
+     *
      * @param servletRequest
      * @param servletResponse
      * @param filterChain
@@ -59,7 +59,10 @@ public class RestAuthFilterHandler extends AntPathMatcher implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         String token = request.getHeader(HttpHeaders.AUTHORIZATION);
-        // TODO 签名
+        if (!verifySignature(request)) {
+            responseFail(servletResponse, "Failed to verify signature");
+            return;
+        }
         // 白名单校验
         if (excludePath.contains(request.getServletPath())) {
             filterChain.doFilter(servletRequest, servletResponse);
@@ -81,27 +84,79 @@ public class RestAuthFilterHandler extends AntPathMatcher implements Filter {
 
     /**
      * return Fail Response Msg
+     *
      * @param response
      * @param message
      * @throws IOException
      */
-    protected void responseFail(HttpServletResponse response, String message) throws IOException {
-        PrintWriter out = response.getWriter();
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    protected void responseFail(ServletResponse servletResponse, String message) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
         JsonMapper jsonMapper = new JsonMapper();
-        out.print(jsonMapper.writeValueAsString(ResponseData.FAIL(message)));
-        out.flush();
+        servletResponse.getWriter().write(jsonMapper.writeValueAsString(ResponseData.FAIL(message, HttpServletResponse.SC_FORBIDDEN)));
+        servletResponse.getWriter().flush();
+        servletResponse.getWriter().close();
     }
 
-    /**
-     * check whitelist
-     * @param request
-     * @return
-     */
-    public boolean checkApiWhitelist(HttpServletRequest request) {
-        // TODO 白名单检查
-        return true;
+    private boolean verifySignature(HttpServletRequest request) {
+        try {
+            String signature = request.getHeader("sign");
+            String timestamp = request.getHeader("timestamp");
+            if (StringUtils.isEmpty(timestamp) || StringUtils.isEmpty(signature)) {
+                return false;
+            }
+            // 1分钟
+            long df = System.currentTimeMillis() - Long.parseLong(timestamp);
+            if (df > 600 || df < 0) {
+                return false;
+            }
+            String paramsStr;
+            if ("POST".equals(request.getMethod()) || "PUT".equals(request.getMethod())) {
+                BufferedReader reader = request.getReader();
+                Map<String, Object> requestMap = objectMapper.readValue(reader, Map.class);
+                // 对Map对象进行排序
+                Map<String, Object> sortedMap = sortMap(requestMap);
+                // 将排序后的Map转换为字符串
+                paramsStr = objectMapper.writeValueAsString(sortedMap);
+            } else {
+                Map<String, String[]> parameterMap = request.getParameterMap();
+                String[] keys = parameterMap.keySet().toArray(new String[0]);
+                Arrays.sort(keys);
+                StringBuilder sb = new StringBuilder();
+                for (String key : keys) {
+                    String value = Arrays.stream(parameterMap.get(key)).findAny().orElse("");
+                    sb.append(key).append(value);
+                }
+                paramsStr = sb.toString();
+            }
+            // 计算签名
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            StringBuilder builder = new StringBuilder();
+            for (byte b : md.digest((paramsStr + timestamp).getBytes())) {
+                builder.append(String.format("%02x", b));
+            }
+            String signatureToVerify = builder.toString();
+            // 比较签名
+            return signatureToVerify.equals(signature);
+        } catch (Exception e) {
+            log.error("签名验证异常！{}", e.getMessage());
+        }
+        return false;
+    }
+
+    private Map<String, Object> sortMap(Map<String, Object> map) {
+        Map<String, Object> sortedMap = new TreeMap<String, Object>();
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if (entry.getValue() instanceof Map) {
+                // 如果Map中的Value也是一个Map类型，则递归进行排序
+                sortedMap.put(entry.getKey(), sortMap((Map<String, Object>) entry.getValue()));
+            } else {
+                sortedMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return sortedMap;
     }
 }
+        
